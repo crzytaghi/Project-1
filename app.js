bbe272148a5790add30985b42905c0603f9b5616
Andrew Taghi <aataghi13@gmail.com>
2020-05-16 17:04:20 -0500

    Completed functionality of Triva Site

--- app.js
+++ app.js
@@ -2,14 +2,11 @@
 
 $(() => {
 
-  // $('button').on('click', (event) => {
-  //   event.preventDefault();
-
 
 
 
     $.ajax({
-      url: 'https://opentdb.com/api.php?amount=10&type=multiple'
+      url: 'https://opentdb.com/api.php?amount=10&type=boolean'
     }).then(
       (data) => {
         // console.log(data.results);
@@ -20,42 +17,37 @@ $(() => {
 
         const openModal = () => {
           $modal.css('display', 'block');
+          $('body').css('background-color', "rgb(0,0,0,0.8)");
+          $('a').css('color', 'white');
           for (let i=0; i<data.results.length; i++) {
-            const $div = $('<div>').addClass(`question`).appendTo('.modal-textbook')
+            const $div = $('<div>').addClass(`question`).appendTo('.modal')
+            // const $question = $('<h3>').text(`Question No. ${i+=1}`).appendTo($div)
             const $h3 = $('<h3>').text(`Category: ${data.results[i].category}`).appendTo($div)
             const $h4 = $('<h4>').text(`Difficulty: ${data.results[i].difficulty}`).appendTo($div)
-            const $h2 = $('<h2>').text(`Question: ${data.results[i].question}`).appendTo($div)
+            const $h2 = $('<h2>').text(`True or False: ${data.results[i].question}`).appendTo($div)
             const $ul = $('<ul>').appendTo($div)
-            const $li = $('<li>').text(data.results[i].incorrect_answers).appendTo($ul);
-            const $li2 = $('<li>').text(data.results[i].correct_answer).appendTo($ul)
+            // const $li = $('<li>').text(data.results[i].incorrect_answers).appendTo($ul);
+            const $answer = $('<button>').text('Click to show answer').appendTo($div)
+            $answer.on('click', (event) => {
+              $(event.currentTarget).text(data.results[i].correct_answer).appendTo($div).off();
+            })
+
           }
         }
 
-        const closeModal = () => {
+        const closeModal = (event) => {
           $modal.css('display', 'none');
+          $('body').css('background-color', 'white');
+          window.location.reload();
         }
 
         $openBtn.on('click', openModal);
         $closeBtn.on('click', closeModal);
-
-        // for (let i=0; i<data.results.length; i++) {
-        //   const $div = $('<div>').addClass(`question`).appendTo('.modal-textbook')
-        //   const $h3 = $('<h3>').text(`Category: ${data.results[i].category}`).appendTo($div)
-        //   const $h4 = $('<h4>').text(`Difficulty: ${data.results[i].difficulty}`).appendTo($div)
-        //   const $h2 = $('<h2>').text(`Question: ${data.results[i].question}`).appendTo($div)
-        //   const $ul = $('<ul>').appendTo($div)
-        //   const $li = $('<li>').text(data.results[i].incorrect_answers).appendTo($ul);
-        //   const $li2 = $('<li>').text(data.results[i].correct_answer).appendTo($ul)
-        // }
-
       },
       (error) => {
         console.error(error);
       }
     )
-// })
-
-
 
 
   $('img').on('click', (event) => {
@@ -65,10 +57,42 @@ $(() => {
     console.log($category);
 
     $.ajax({
-      url: `https://opentdb.com/api.php?amount=10&category=${$category}`
+      url: `https://opentdb.com/api.php?amount=10&type=boolean&category=${$category}`
     }).then(
       (data) => {
         console.log(data);
+
+        const $openBtn = $('img');
+        const $modal = $('.modal');
+        const $closeBtn = $('#close');
+
+        const openModal = () => {
+          $modal.css('display', 'block');
+          $('body').css('background-color', "rgb(0,0,0,0.8)");
+          $('a').css('color', 'white');
+          for (let i=0; i<data.results.length; i++) {
+            const $div = $('<div>').addClass(`question`).appendTo('.modal')
+            // const $question = $('<h3>').text(`True or False No. ${i+=1}`).appendTo($div)
+            const $h3 = $('<h3>').text(`Category: ${data.results[i].category}`).appendTo($div)
+            const $h4 = $('<h4>').text(`Difficulty: ${data.results[i].difficulty}`).appendTo($div)
+            const $h2 = $('<h2>').text(`True or False: ${data.results[i].question}`).appendTo($div)
+            const $ul = $('<ul>').appendTo($div)
+            // const $li = $('<li>').text(data.results[i].incorrect_answers).appendTo($ul);
+            const $answer = $('<button>').text('Click to show answer').appendTo($div)
+            $answer.on('click', (event) => {
+              $(event.currentTarget).text(data.results[i].correct_answer).appendTo($div).off();
+            })
+          }
+        }
+
+        const closeModal = (event) => {
+          $modal.css('display', 'none');
+          $('body').css('background-color', 'white');
+          window.location.reload();
+        }
+
+        $openBtn.on('click', openModal);
+        $closeBtn.on('click', closeModal);
       },
       (error) => {
         console.error(error);
